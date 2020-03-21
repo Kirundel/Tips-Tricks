246d7fd81f61f84167d48204117ac6cbc77aa601
Kirundel <kirill.kaymakov@gmail.com>
2020-03-21 09:16:06 +0300

    Minor changes

--- project/cnd/ocr/train_script.py
+++ project/cnd/ocr/train_script.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
     transforms = get_transforms(device)
 
-    dataset = CommonDataset(transforms = transforms, cached=False)
+    dataset = CommonDataset(transforms=transforms, cached=False)
     train_dataset = dataset.Train
 
     train_loader = DataLoader(
